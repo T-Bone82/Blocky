d0fd42d0619283acde986f388bf73c13b572f35e
Jean-François Tremblay <tremblay.jf@gmail.com>
2018-09-28 23:22:28 -0400

    First commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "ObstacleCube")
+        {
+            Debug.Log("You lose");
+            movement.rb.AddForce(0, 600 * Time.deltaTime, -500 * Time.deltaTime, ForceMode.Impulse);
+            movement.enabled = false;
+            
+        }
+        if (collision.collider.tag == "TheEnd")
+        {
+            Debug.Log("You win");
+            movement.rb.AddForce(0, 600 * Time.deltaTime, -500 * Time.deltaTime, ForceMode.Impulse);
+            movement.enabled = false;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public Transform player;
+    private bool notificationPublished = false;
+    private bool resetPosition = false;
+    private float resetTime = 0f;
+    private bool pushedBack = false;
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (resetPosition && (Time.fixedTime - resetTime) > 5)
+        {
+            resetPosition = false;
+            Debug.Log("set resetPosition to false");
+        }
+
+        if (resetPosition && !pushedBack)
+        {
+            rb.AddForce(0, 0, -2000 * Time.deltaTime, ForceMode.Impulse);
+            Debug.Log("BANG");
+            pushedBack = true;
+            this.enabled = false;
+        }
+        else if (!resetPosition)
+        {
+            //Debug.Log("Go for it JACK!");
+            //this.enabled = true;
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (player.position.y < -10 && !notificationPublished)
+        {
+            Debug.Log("You fell in the hole");
+            notificationPublished = true;
+            resetPosition = true;
+            resetTime = Time.fixedTime;
+            transform.position = new Vector3(0, 5f, -5.76f);
+        }
+    }
+}
