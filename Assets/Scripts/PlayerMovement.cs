f3f944c83712c935b9c47693cdbc9f53c71c39fe
Jean-François Tremblay <tremblay.jf@gmail.com>
2018-10-02 23:15:55 -0400

    rajouter le score et high score. reste a fixer problème de reset du score quand restart.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour {
 
@@ -11,48 +12,68 @@ public class PlayerMovement : MonoBehaviour {
     private bool notificationPublished = false;
     private bool resetPosition = false;
     private float resetTime = 0f;
-    
+    private Text txtPoints;
+    private Text txtHighScore;
+
+    private void Awake()
+    {
+        txtPoints = GameObject.Find("Points").GetComponent<Text>();
+        txtHighScore = GameObject.Find("HighScore").GetComponent<Text>();
+    }
+
     // Update is called once per frame
     void FixedUpdate () {
-        if (resetPosition)
+        // Verify if situation of restart
+        if (getResetPosition())
         {
             if ((Time.fixedTime - resetTime) > 5)
             {
-                resetPosition = false;
+                // After 5 seconds let it be!
+                setResetPosition(false);
                 notificationPublished = false;
             }        
         }
         else
-        {
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        }
-            
-        
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Let advance the player
+           
+        // Reading registered keys and searching for "d" or "a"
         if (Input.GetKey("d"))
-        {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
         if (Input.GetKey("a"))
-        {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
+        
+        // Reset position and restart if player if falling off the cliff
         if (player.position.y < -10 && !notificationPublished)
-        {
             restart();
+    }
+
+    private void UpdateScores()
+    {
+        int score = 0;
+        int highScore = 0;
+        int.TryParse(txtPoints.text, out score);
+        if (int.TryParse(txtPoints.text, out score) && int.TryParse(txtHighScore.text, out highScore))
+        {
+            if (score > highScore)
+                txtHighScore.text = txtPoints.text;
         }
+        //txtPoints.text = "0";
     }
 
     public void restart()
     {
         notificationPublished = true;
-        resetPosition = true;
+        setResetPosition(true);
         resetTime = Time.fixedTime;
 
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
 
-        transform.position = new Vector3(-3, 5f, -5.76f);
+        transform.position = new Vector3(-3, 5f, 0f);
+        UpdateScores();
     }
+
+    public bool getResetPosition() { return resetPosition; }
+    public void setResetPosition(bool value) { resetPosition = value; }
 
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    private GameObject objPlayer;
+    private PlayerMovement movement;
+    private Transform player;
+    private float score = 0f;
+    private Text txtPoints;
+    private Text txtHighScore;
+
+    public float getScore() { return score; }
+    public void setScore(float value) { score = value; }
+
+    private void Awake()
+    {
+        txtPoints = GameObject.Find("Points").GetComponent<Text>();
+        txtHighScore = GameObject.Find("HighScore").GetComponent<Text>();
+        objPlayer = GameObject.Find("Player");
+
+        if (objPlayer != null)
+        {
+            // Getting the component inside the Player object
+            movement = objPlayer.GetComponent<PlayerMovement>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (movement.getResetPosition())
+            txtPoints.text = "0";
+        if (movement != null) {
+            if (movement.player.position.z > getScore())
+            {
+                setScore(movement.player.position.z);
+            }
+            txtPoints.text = ((int) getScore()).ToString();
+        }
+    }
+}
