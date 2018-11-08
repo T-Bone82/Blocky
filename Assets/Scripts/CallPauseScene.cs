93dae7dc339495ba10e24973f2f640ce88eb8e89
Jean-François Tremblay <tremblay.jf@gmail.com>
2018-11-07 23:20:41 -0500

    travailler le menu pause

---
+++ Assets/Scripts/CallPauseScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CallPauseScene : MonoBehaviour {
+
+    private GameObject pauseObject;
+
+    public void Awake()
+    {
+        pauseObject = GameObject.Find("PausePrefab").GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            pauseObject.SetActive(true);
+        }	
+	}
+}
---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour {
+
+    private GameObject pauseObject;
+
+    public void Awake()
+    {
+        pauseObject = GameObject.Find("PausePrefab").GetComponent<GameObject>();
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseObject.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0f;
+            pauseObject.SetActive(true);
+        }
+    }
+}
