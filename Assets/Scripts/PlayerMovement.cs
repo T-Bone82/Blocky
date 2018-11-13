89e08e16cf37c0fa8be19eb12dfc7682ff40180c
Jean-François Tremblay <tremblay.jf@gmail.com>
2018-11-12 23:52:44 -0500

    optimisation, conversion d'object en prefab et changer les couleur et arranger la ligne d'arriver, et controler la vélocité à l'arrivée

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,45 +6,37 @@ public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
     public Transform player;
-
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
 
-    private bool notificationPublished = false;
-    private bool resetPosition = false;
-    private float resetTime = 0f;
     private TextMeshProUGUI txtPoints;
     private TextMeshProUGUI txtHighScore;
-    private bool theEnd = false;
+    private Transform transformPoteau1;
+    private Transform transformPoteau2;
 
-    private void Awake()
+    private float resetTime = 0f;
+   
+    private bool theEnd = false;
+    private bool notificationPublished = false;
+    private bool resetPosition = false;
+    private bool stoppedSpaceShip = false;
+
+    void Awake()
     {
         txtPoints = GameObject.Find("Points").GetComponent<TextMeshProUGUI>();
         txtHighScore = GameObject.Find("HighScore").GetComponent<TextMeshProUGUI>();
+        transformPoteau1 = GameObject.Find("poteauFin1").GetComponent<Transform>();
+        transformPoteau2 = GameObject.Find("poteauFin2").GetComponent<Transform>();
         restart();
     }
 
-    // Update is called once per frame
-    void FixedUpdate () {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector3(0, 0, 1);
-            //rb.ResetCenterOfMass();
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            //rb.velocity = new Vector3(0, 0, 1);
-            rb.AddForce(new Vector3(0, 10, 0));
-            //rb.ResetCenterOfMass();
-        }
-
-
+    void FixedUpdate ()
+    {
         if (!theEnd)
         {
-            
             keepMoving();
         }
-        else
+        else 
         {
             showSuccessMenu();
         }
@@ -52,23 +44,30 @@ public class PlayerMovement : MonoBehaviour {
 
     private void showSuccessMenu()
     {
-
+        if (!stoppedSpaceShip)
+        {
+            stoppedSpaceShip = true;
+            rb.velocity = new Vector3(0, 0, 20);
+        }
     }
 
     private void keepMoving() {
         // Verify if situation of restart
         if (getResetPosition())
         {
+            // After 5 seconds let it be!
             if ((Time.fixedTime - resetTime) > 5)
             {
-                // After 5 seconds let it be!
                 setResetPosition(false);
                 notificationPublished = false;
             }
         }
         else
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Let advance the player
-
+        {
+            // Let advance the player
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+            
         // Reading registered keys and searching for "d" or "a"
         if (Input.GetAxis("Horizontal") > 0)
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
@@ -78,16 +77,16 @@ public class PlayerMovement : MonoBehaviour {
         // Reset position and restart if player if falling off the cliff
         if (player.position.y < -10 && !notificationPublished)
             restart();
+
         // Update score with an integer number
         txtPoints.text = ((int)player.position.z).ToString();
         
+        // Is it the end?
         theEnd = isEnd();
     }
 
     private bool isEnd()
     {
-        Transform transformPoteau1 = GameObject.Find("poteauFin1").GetComponent<Transform>();
-        Transform transformPoteau2 = GameObject.Find("poteauFin2").GetComponent<Transform>();
         return 
             player.position.x > transformPoteau2.position.x 
             && player.position.x < transformPoteau1.position.x
